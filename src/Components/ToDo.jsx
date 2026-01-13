6dda7a2a5cedf2e9dd6b697cfcdb40faf0ea584f
toruthecoder <devishsage@gmail.com>
2026-01-13 21:45:21 +0500

    push

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -5,7 +5,7 @@ function ToDo() {
         <>
             <div className='noti absolute  w-full h-full z-100 hidden'>
                 <div
-                    className='get hidden relative w-[300px] top-[50%] left-[40%] bg-[white] px-10 py-10 z-100 text-center flex flex-col rounded-[10px]'>
+                    className='get hidden relative w-75 top-[50%] left-[40%] bg-[white] px-10 py-10 z-100 text-center flex flex-col rounded-[10px]'>
                     <p>Are You Sure?</p>
                     <div className='flex gap-2 mt-4 justify-center'>
                         <button className='yesBtn bg-red-400 rounded-[5px] cursor-pointer px-5 text-white py-1'>Delete</button>
