e60d2466912e2b0785152189569aa4dc9870291f
toruthecoder <devishsage@gmail.com>
2026-01-30 14:38:45 +0500

    ooo

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_CLIENT_URL}/api` : "/api";
+// const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_CLIENT_URL}/api` : "/api";
+
+const BASE_URL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:3002/api"
+        : `${import.meta.env.VITE_CLIENT_URL}/api`;
 
 const api = axios.create({
     baseURL: BASE_URL,
