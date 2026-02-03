3eeb1df2a55b09f507ac6f3613984a743094ca9b
toruthecoder <devishsage@gmail.com>
2026-02-03 14:58:43 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './Pages/login.jsx'
 import Signup from './Pages/signup.jsx'
 import Home from './Pages/home.jsx'
 import AuthRedirect from "./Components/authRedirect.jsx";
+import NotFound from "./Components/notFound.jsx";
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<AuthRedirect><Login /></AuthRedirect>} />
       <Route path="/signup" element={<AuthRedirect><Signup /></AuthRedirect>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
---
+++ src/Components/notFound.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NotFound() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen ">
+            <h1 className="mb-4 text-3xl  font-bold text-black">
+                404 Page Not Found
+            </h1>
+            <h2 className="mb-8 text-3xl  text-black">
+                You Got Lost Traveller Return Back!
+            </h2>
+            <Link
+                to="/"
+                className="border rounded-md bg-black p-3 text-white hover:bg-[#494d4f] duration-500 cursor-pointer"
+            >
+                Go Back
+            </Link>
+        </div>
+    );
+}
+
+export default NotFound;
